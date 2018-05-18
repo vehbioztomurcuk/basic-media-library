7e83cf8d5d369d023a5ef2ed1d43c175eb66096c
Vehbi Oztomurcuk <vehbi.oztomurcuk@gmail.com>
2018-05-18 17:45:10 +0300

    up

---
+++ catalog.php
@@ -0,0 +1,71 @@
+<?php 
+
+include("inc/data.php");
+include("inc/functions.php");
+$pageTitle = "Full Catalog";
+$section = null;
+
+if (isset($_GET["cat"]))  {  
+    if ($_GET["cat"] == "books") {
+        $pageTitle = "Books";
+        $section = "books";
+    } else if ($_GET["cat"] == "movies") {
+        $pageTitle = "Movies";
+        $secton = "movies";
+        $section = "movies";
+    } else if ($_GET["cat"] == "music") {
+        $pageTitle = "Music";
+        $secton = "music";
+        $section = "music";
+    } 
+    
+}
+
+
+include("inc/header.php"); ?>
+ 
+
+<div class="section catalog page">
+
+<div class="Wrapper"> 
+
+<h1><?php 
+if($section != null){
+    echo "<a href='catalog.php'> Full Catalog  </a> &gt; ";
+} 
+    echo $pageTitle;
+
+
+ ?></h1>
+<ul class="items";>
+
+<?php $categories  = array_category($catalog, $section);
+						foreach ($categories as $id) {
+                         echo get_item_html($id, $catalog[$id]);
+						}
+
+
+ ?>
+
+</ul>
+
+</div>
+
+
+
+
+</div>
+
+        <?php include("inc/footer.php"); ?>
+
+
+        
+
+
+
+
+
+
+
+        
+  ?>
---
+++ inc/footer.php
@@ -0,0 +1,21 @@
+</div>  <!-- End content -->
+ 
+	</div>
+
+	<div class="footer">
+
+		<div class="wrapper">
+
+			<ul>		
+				<li><a href="http://twitter.com/vehbioztomurcuk">Twitter</a></li>
+				<li><a href="https://www.facebook.com/TeamTreehouse">Facebook</a></li>
+			</ul>
+
+			<p>&copy; <?=date("Y");?> Personal Media Library</p>
+
+		</div>
+	
+	</div>
+
+</body>
+</html>
